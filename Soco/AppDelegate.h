43d10c80a42b9c86b7ddf5355c6d3c00ea5e46f7
jiliszc <jili_baoer.std@163.com>
2018-07-02 16:59:48 +0800

    Initial Commit

---
+++ Soco/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Soco
+//
+//  Created by jili on 2018/7/2.
+//  Copyright © 2018年 jili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Soco/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Soco
+//
+//  Created by jili on 2018/7/2.
+//  Copyright © 2018年 jili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
